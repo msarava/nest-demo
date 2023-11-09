dfa97ef396ecdcfe83a78f4d611b92d96c6caf64
Antoine Dabonneville <antoine.dabonneville@sogeti.com>
2023-11-09 16:36:27 +0100

    [REPOSITORY] add repo and dependency injection

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+} from '@nestjs/common';
+import { createMessageDTO } from './dtos/create-message.dto';
+import { MessagesService } from './messages.services';
 
-@Controller('messages')
-export class MessagesController {}
+@Controller('/messages')
+export class MessagesController {
+  y;
+  constructor(public messageService: MessagesService) {}
+
+  @Get()
+  getAllMessages() {
+    return this.messageService.findAll();
+  }
+
+  @Post()
+  createMessage(@Body() body: createMessageDTO) {
+    return this.messageService.create(body.content);
+  }
+
+  @Get('/:id')
+  async getMessageById(@Param('id') id: string) {
+    const message = await this.messageService.findOne(id);
+
+    if (!message) {
+      throw new NotFoundException('message not found');
+    }
+    return message;
+  }
+}
